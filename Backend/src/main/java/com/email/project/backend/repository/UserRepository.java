dec5d98c653b54db717e3a103d57c873ac2adb67
Duc Lam <dinhduclam2001@gmail.com>
2023-06-07 20:03:39 +0700

    User edit DTO

--- Backend/src/main/java/com/email/project/backend/repository/UserRepository.java
+++ Backend/src/main/java/com/email/project/backend/repository/UserRepository.java
@@ -1,10 +1,8 @@
 package com.email.project.backend.repository;
 
-import com.email.project.backend.dto.UserView;
+import com.email.project.backend.dto.user.UserView;
 import com.email.project.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     UserView getUserViewById(int id);
