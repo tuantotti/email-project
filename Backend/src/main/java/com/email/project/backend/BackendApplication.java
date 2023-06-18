713dcee18b6d39e440b90bdf12bed6b122136df5
tuantotti <tuantotti14032001@gmail.com>
2023-06-18 09:26:46 +0700

    setup docker compose

--- Backend/src/main/java/com/email/project/backend/BackendApplication.java
+++ Backend/src/main/java/com/email/project/backend/BackendApplication.java
@@ -4,6 +4,7 @@ import com.email.project.backend.configuration.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
