3938c7153f22c4a8c35a526213bcd93b2fb270e9
Duc Lam <dinhduclam2001@gmail.com>
2023-06-24 13:56:30 +0700

    define & apply response structure for all api

--- Backend/src/main/java/com/email/project/backend/controller/MailController.java
+++ Backend/src/main/java/com/email/project/backend/controller/MailController.java
@@ -2,16 +2,14 @@ package com.email.project.backend.controller;
 
 import com.email.project.backend.constant.MailStatus;
 import com.email.project.backend.dto.MailDto;
+import com.email.project.backend.dto.Response;
 import com.email.project.backend.entity.Mail;
 import com.email.project.backend.service.MailService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "Mail API", description = "The api for mail operation")
 @RestController
@@ -25,17 +23,22 @@ public class MailController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<MailDto>> getMail(@RequestParam MailStatus status, Pageable pageable) {
+    public Response<Page<MailDto>> getMail(@RequestParam MailStatus status, Pageable pageable) {
         Page<MailDto> mailDtoList = mailService.getMail(status, pageable);
 
-        return ResponseEntity.ok(mailDtoList);
+        return Response.ok(mailDtoList);
     }
 
-    @PostMapping
-    public ResponseEntity<MailDto> sendMail(@RequestBody MailDto mailDto) {
-        MailDto sentMail = mailService.sendMail(mailDto);
-
-        return ResponseEntity.ok(sentMail);
+    @PostMapping("send")
+    public Response<MailDto> sendMail(@RequestBody MailDto mailDto) {
+        try{
+            MailDto mail = mailService.sendSimpleMail(mailDto);
+            return Response.ok(mail, "Email sent successfully!");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return Response.fail("Something went wrong!");
+        }
     }
 
     @PutMapping("/status")
