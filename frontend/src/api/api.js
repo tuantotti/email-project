6aaf35d5f5acbdae4970824f8d216b5d5e8e6c42
dungnd193 <dungnd.work@gmail.com>
2023-06-26 11:53:53 +0700

    view mail detail feature

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,8 @@ const API = {
     LOGIN: '/api/login',
     SIGN_UP: '/api/signup',
     GET_MAIL: '/api/mail',
-    STAR_MAIL: '/api/mail/status'
+    STAR_MAIL: '/api/mail/status',
+    DOWNLOAD_FILE: '/api/mail/file',
 }
 
 export default API
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import authenticationSlice from './slices/authenticationSlice'
 import getMailsSlice from './slices/getMailsSlice'
 import starMailSlice from './slices/starMailSlice'
+import viewMailSlice from './slices/viewMailSlice'
 const reducer = {
     authenticationSlice,
     getMailsSlice,
-    starMailSlice
+    starMailSlice,
+    viewMailSlice
 };
 
 export const store = configureStore({ reducer })
