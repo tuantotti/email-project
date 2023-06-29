f7c09323bf8aef0dded82d1a44af565301169fa9
tuantotti <tuantotti14032001@gmail.com>
2023-06-29 07:31:24 +0700

    send email with attachment

--- SMTPServer/src/main/java/com/smtp/server/server/MailSaver.java
+++ SMTPServer/src/main/java/com/smtp/server/server/MailSaver.java
@@ -1,75 +1,142 @@
 package com.smtp.server.server;
 
-import com.smtp.server.model.EmailModel;
+import com.smtp.server.model.FileData;
+import com.smtp.server.model.Mail;
+import com.smtp.server.model.MailStatus;
+import com.smtp.server.service.MailService;
+import com.sun.mail.util.BASE64DecoderStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.nio.charset.Charset;
-import java.util.Date;
+import javax.mail.BodyPart;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public final class MailSaver extends Observable {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MailSaver.class);
 
     public void saveEmailAndNotify(String from, String to, InputStream data) {
-        String mailContent = convertStreamToString(data);
-        LOGGER.info("Send email from {} to {} data {}", from, to, mailContent);
+        Mail emailModel;
 
-        EmailModel model = new EmailModel();
-        model.setFrom(from);
-        model.setTo(to);
-        model.setSubject(getSubjectFromStr(mailContent));
-        model.setEmailStr(mailContent);
+        try {
+            MimeMessage mimeMessage = toMimeMessage(data);
+            emailModel = convertToMail(from, to, mimeMessage);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        LOGGER.info("Send email from {} to {}", from, to);
 
         synchronized (this) {
-//            String filePath = saveEmailToFile(mailContent);
-
-            model.setReceivedDate(new Date());
-//            model.setFilePath(filePath);
-
             setChanged();
-            notifyObservers(model);
+            notifyObservers(emailModel);
         }
     }
 
-    private String convertStreamToString(InputStream is) {
-        final long lineNbToStartCopy = 4; // Do not copy the first 4 lines (received part)
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-        StringBuilder sb = new StringBuilder();
-
-        String line;
-        long lineNb = 0;
+    private MimeMessage toMimeMessage(InputStream inputStream) {
         try {
-            while ((line = reader.readLine()) != null) {
-                if (++lineNb > lineNbToStartCopy) {
-                    sb.append(line).append('\n');
-                }
-            }
+            Session session = Session.getDefaultInstance(System.getProperties());
+            byte[] inputByte = inputStream.readAllBytes();
+
+            MimeMessage mimeMessage = new MimeMessage(session, new ByteArrayInputStream(inputByte));
+
+            return mimeMessage;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
-            LOGGER.error("", e);
+            throw new RuntimeException(e);
         }
-        return sb.toString();
     }
 
-    private String getSubjectFromStr(String data) {
-        try {
-            BufferedReader reader = new BufferedReader(new StringReader(data));
+    private String getTextFromMimeMultipart(MimeMultipart mimeMultipart) throws Exception {
+        String result = "";
+        int count = mimeMultipart.getCount();
+        for (int i = 0; i < count; i++) {
+            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+            if (bodyPart.isMimeType("text/plain")) {
+                result = result + bodyPart.getContent();
+                break; // without break same text appears twice in my tests
+            } else if (bodyPart.isMimeType("text/html")) {
+                String html = (String) bodyPart.getContent();
+                result = result + html;
+            } else if (bodyPart.getContent() instanceof MimeMultipart) {
+                result = result + getTextFromMimeMultipart((MimeMultipart) bodyPart.getContent());
+            }
+        }
+        return result;
+    }
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                Matcher matcher = Pattern.compile("^Subject: (.*)$").matcher(line);
-                if (matcher.matches()) {
-                    return matcher.group(1);
+    private Mail convertToMail(String from, String to, MimeMessage mimeMessage) {
+        try {
+            Mail emailModel = new Mail();
+            emailModel.setFromAddress(from);
+            emailModel.setToAddress(to);
+            emailModel.setSubject(mimeMessage.getSubject());
+            emailModel.setSendDate(mimeMessage.getSentDate());
+            emailModel.setReceivedDate(mimeMessage.getReceivedDate());
+            emailModel.setStatus(MailStatus.INBOX);
+
+            List<FileData> fileDataList = new ArrayList<>();
+
+            if (mimeMessage.isMimeType("text/plain")) {
+                emailModel.setBody(mimeMessage.getContent().toString());
+            } else if (mimeMessage.isMimeType("multipart/*")) {
+                MimeMultipart mimeMultipart = (MimeMultipart) mimeMessage.getContent();
+                String body = getTextFromMimeMultipart(mimeMultipart);
+                emailModel.setBody(body);
+            }
+
+            Multipart mp = (Multipart) mimeMessage.getContent();
+            int count = mp.getCount();
+            for (int i = 0; i < count; i++) {
+                BodyPart bp = mp.getBodyPart(i);
+                // get base64 decode of file and save to file system
+                if (bp.getContent() instanceof BASE64DecoderStream) {
+                    InputStream is = (InputStream) bp.getContent();
+                    String fileName = bp.getFileName();
+                    int extensionIndex = fileName.lastIndexOf('.');
+                    String name = fileName.substring(0, extensionIndex) + "_" + System.currentTimeMillis() + fileName.substring(extensionIndex);
+                    String filePath = MailService.getFolderPath() + File.separator + name;
+                    File file = new File(filePath);
+                    // save file to
+                    Files.copy(is, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+                    // store file properties in db
+                    FileData fileData = FileData.builder()
+                            .name(name)
+                            .type(bp.getContentType())
+                            .size(file.length())
+                            .filePath(filePath)
+                            .build();
+                    fileDataList.add(fileData);
                 }
             }
+
+            emailModel.setFileDataList(fileDataList);
+            fileDataList.stream().forEach(fileData -> fileData.setMail(emailModel));
+            MailService.save(emailModel);
+
+            return emailModel;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
-            LOGGER.error("", e);
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        return "";
     }
 
 }
